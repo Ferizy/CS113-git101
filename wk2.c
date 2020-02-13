b7019a1ea53441c4350a78346e8ed26de8f03599
Soottipong <soottipong.t@ku.th>
2020-02-13 22:22:55 +0700

    Basic

---
+++ wk2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+// #define for "loop"
+//float e;
+//char r[] = "Kaze";
+int main()
+    {
+        //printf("%20s", for );
+        // printf("Hello World!")
+        int a = 40;
+        char b = 'a';
+        char c = 97;
+        float f = 3.14;
+        double d =20.2;
+        // printf("%f %lf",f ,d  );
+        //float e;
+        //printf("%f ",e + 1000);
+        // int ans1 = a + b;
+        // float ans2 = f+ b ; 
+        // printf("%d \n%f\n%c",ans1 ,ans2 ,a * b);
+        float ans3 = pow(15,2);
+        printf("%f",ans3);
+
+
+    }
