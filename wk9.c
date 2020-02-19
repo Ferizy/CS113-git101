7d538231a399e3a3e5c9fb49aa13d4e78e504cbc
Soottipong <soottipong.t@ku.th>
2020-02-19 18:40:18 +0700

    git merge

---
+++ wk9.c
@@ -0,0 +1,14 @@
+git --version                               // เช็คเวอร์ขั่น
+git init                                    // เริ่มใช้กิท
+git add "name". นาสกุล                     
+git config user.name "Name"                 // ใส่ชื่อ
+git config user.email "mail@ku.th"          // ใส่เมล
+git commit -m "message"                     // Commit 
+git log --all --decorate --oneline --graph  // ดูเช็คพ้อย
+git push -u origin branch-name              // นำไฟล์เข้ากิทฮํบ
+git clone <git-url> "ชื่อโฟลเดอร์ใหม่"                      // สร้างไฟล์จาก github
+git branch                                  // ดู branch ทั้งหมด
+git branch branch-name                      // สร้าง branch ตามด้วยชื่อ
+git checkout branch-name                    // เปลี่ยน branch
+git merge branch-name                       // รวมไฟล์
+git pull origin branch-name                 // ดึงไฟล์จาก ชื่อ branch
